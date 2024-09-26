5f527fc7de5180b18e75da404646228add13c4a6
Nora <noraknassir@gmail.com>
2024-09-26 16:45:45 +0200

    corrected linting issues

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -6,8 +6,8 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
-class Command(BaseCommand):
 
+class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
@@ -20,4 +20,3 @@ class Command(BaseCommand):
                 self.stdout.write('Database not available, wait 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database is available'))
-        
