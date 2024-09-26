a21123424b7a22db47b09b5850ac3aa6404c1cb2
Nora <noraknassir@gmail.com>
2024-09-26 16:13:46 +0200

    created core app. added unittest for wait_for_db command

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,8 @@
+"""Django command to wait for database"""
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        pass
