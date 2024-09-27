639bd40cc3704f2139daa070e343259e8dbf4129
Nora <noraknassir@gmail.com>
2024-09-27 12:08:28 +0200

    added custom page in admin for changing user

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Admin customisation
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -10,6 +11,21 @@ from core import models
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Login information'), {'fields': ('last_login',)})
+    )
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
